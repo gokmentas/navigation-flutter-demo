7ec3df05da6a8dd3a7f9e41a1abe1b941889e851
gokmentas <56593104+gokmentas@users.noreply.github.com>
2024-04-15 21:15:21 +0300

    Update screen0.dart

--- lib/screen0.dart
+++ lib/screen0.dart
@@ -8,7 +8,7 @@ class Screen0 extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.purple,
-        title: const Text("Screen "),
+        title: const Text("Screen 0"),
       ),
       body: Center(
         child: Column(
